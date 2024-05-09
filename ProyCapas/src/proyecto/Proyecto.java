9c01bbbae71bfc69b01d5dcd606a6bad58848b94
Soffivp <psvire@utpl.edu.ec>
2024-05-08 22:56:53 -0500

    prueba

--- ProyCapas/src/proyecto/Proyecto.java
+++ ProyCapas/src/proyecto/Proyecto.java
@@ -14,7 +14,7 @@ public class Proyecto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code Pruebaaa
+        // TODO code Pruebaaajjjj
     }
     
 }
